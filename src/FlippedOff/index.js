729c06b4b667d8ae177a731171a32fda240a04c4
Wilson Collado <colladow@gmail.com>
2020-03-18 21:30:46 -0400

    Adding a basic layout.

--- src/FlippedOff/index.js
+++ src/FlippedOff/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './GlobalStyles';
+import Screen, { CardDisplay, Nav } from './Screen';
 
 const FlippedOff = () => (
-  <h1>Hello world!</h1>
+  <ThemeProvider theme={{}}>
+    <React.Fragment>
+      <GlobalStyles />
+
+      <Screen>
+        <CardDisplay>
+          <h1>Current Card</h1>
+        </CardDisplay>
+
+        <Nav>
+          <h1>Nav</h1>
+        </Nav>
+      </Screen>
+    </React.Fragment>
+  </ThemeProvider>
 );
 
 export default FlippedOff;
