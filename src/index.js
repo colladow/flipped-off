38de879463b6a01b82d93e71f82bef49bf167144
Wilson Collado <colladow@gmail.com>
2020-05-25 18:34:21 -0400

    Adding a smooth scroll polyfill to have smooth scrolling on ios safari.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import smoothscroll from 'smoothscroll-polyfill';
 
 import FlippedOff from './FlippedOff';
+
+smoothscroll.polyfill();
 
 ReactDOM.render(<FlippedOff />, document.querySelector('#render'));
 
